21c17f0832a2d8c60b49b356dae44d840bd2f61a
WDB40 <wbrown1640@gmail.com>
2018-09-12 17:15:10 -0500

    Connecting the WordController addWord mapping to addWord view

--- lantern-words/src/main/java/core/controller/WordController.java
+++ lantern-words/src/main/java/core/controller/WordController.java
@@ -1,7 +1,13 @@
 package core.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WordController {
+
+    @GetMapping("/addWord")
+    public String addWord() {
+        return "addWord";
+    }
 }
