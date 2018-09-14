c80ee1eb03f9e34e634260b2748e9328c33917f5
WDB40 <wbrown1640@gmail.com>
2018-09-13 22:17:56 -0500

    Adding the tests for the setters.

--- lantern-words/src/test/java/core/model/WordTest.java
+++ lantern-words/src/test/java/core/model/WordTest.java
@@ -6,16 +6,21 @@ import static org.junit.Assert.*;
 
 public class WordTest {
 
-    Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
+
 
     @Test
     public void getWord() {
 
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
         assertEquals("Hyper", word.getWord());
     }
 
     @Test
     public void setWord() {
+
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
+        word.setWord("After");
+        assertEquals("After", word.getWord());
     }
 
     @Test
@@ -25,28 +30,42 @@ public class WordTest {
     @Test
     public void getDefinition() {
 
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
         assertEquals("To be excited", word.getDefinition());
     }
 
     @Test
     public void setDefinition() {
+
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
+        word.setDefinition("To be hype");
+        assertEquals("To be hype", word.getDefinition());
     }
 
     @Test
     public void getUsedInSentence() {
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
         assertEquals("I am hyper", word.getUsedInSentence());
     }
 
     @Test
     public void setUsedInSentence() {
+
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
+        word.setUsedInSentence("I am not hyper");
+        assertEquals("I am not hyper", word.getUsedInSentence());
     }
 
     @Test
     public void getPronunciation() {
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
         assertEquals("hi-per", word.getPronunciation());
     }
 
     @Test
     public void setPronunciation() {
+        Word word = new Word("Hyper", "To be excited", "I am hyper","hi-per");
+        word.setPronunciation("high-pur");
+        assertEquals("high-pur", word.getPronunciation());
     }
 }
