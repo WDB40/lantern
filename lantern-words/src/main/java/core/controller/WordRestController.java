05a382d6ccd8cd2f8dcfcda928bf5ec5a5c7fe59
WDB40 <wbrown1640@gmail.com>
2018-09-14 21:02:07 -0500

    Creating the API for addWord.

--- lantern-words/src/main/java/core/controller/WordRestController.java
+++ lantern-words/src/main/java/core/controller/WordRestController.java
@@ -1,8 +1,29 @@
 package core.controller;
 
+import core.model.Word;
+import core.model.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/API")
 @RestController
 public class WordRestController {
+
+    @Autowired
+    WordRepository wordRepository;
+
+//    See if we can get this to work with the requestParam of just the word object.
+
+    @RequestMapping("/addWord")
+    public Word addWord(@RequestParam String word, @RequestParam String definition,
+                        @RequestParam String usedInSentence, @RequestParam String pronunciation){
+
+        Word newWord = new Word(word, definition, usedInSentence, pronunciation);
+        wordRepository.save(newWord);
+
+        return newWord;
+    }
 
 }
