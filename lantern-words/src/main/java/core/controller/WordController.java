14fcda7d08b27085916053d047216d5bec6555d5
WDB40 <wbrown1640@gmail.com>
2018-09-16 20:09:17 -0500

    Adding connection to successfulWordAdd view on successful word add.

--- lantern-words/src/main/java/core/controller/WordController.java
+++ lantern-words/src/main/java/core/controller/WordController.java
@@ -30,7 +30,7 @@ public class WordController {
         }
         else{
             wordRepository.save(word);
-            return "Good";
+            return "successfulWordAdd";
         }
     }
 }
