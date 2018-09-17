23f60461b9f38c1efa5be518f6ccdc27a0305b26
WDB40 <wbrown1640@gmail.com>
2018-09-16 21:08:46 -0500

    Creating the WordView class, test, and controller. Empty shells right now.

---
+++ lantern-view-word/src/main/java/core/model/WordView.java
@@ -0,0 +1,11 @@
+package core.model;
+
+public class WordView {
+
+    private String wordName;
+    private String definition;
+    private String usedInSentence;
+    private String pronunciation;
+
+
+}
---
+++ lantern-view-word/src/test/java/core/model/WordViewTest.java
@@ -0,0 +1,7 @@
+package core.model;
+
+import static org.junit.Assert.*;
+
+public class WordViewTest {
+
+}
