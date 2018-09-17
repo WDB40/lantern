6822d875ff1f8f21528c977e8a4fd3e167ed6255
WDB40 <wbrown1640@gmail.com>
2018-09-16 19:55:41 -0500

    Moving addWord Post method to the WordController. Removed it from WordRestController.

--- lantern-words/src/main/java/core/controller/WordController.java
+++ lantern-words/src/main/java/core/controller/WordController.java
@@ -1,14 +1,35 @@
 package core.controller;
 
 import core.model.Word;
+import core.model.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class WordController {
 
+    @Autowired
+    WordRepository wordRepository;
+
     @GetMapping("/addWord")
     public String addWord(Word word) {
         return "addWord";
+    }
+
+    @PostMapping("/addWord")
+    public String addWord(@Valid Word word, BindingResult result){
+
+        if(result.hasErrors()){
+            return "addWord";
+        }
+        else{
+            wordRepository.save(word);
+            return "Good";
+        }
     }
 }
--- lantern-words/src/main/java/core/controller/WordRestController.java
+++ lantern-words/src/main/java/core/controller/WordRestController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -15,29 +14,4 @@ import javax.validation.Valid;
 @RestController
 public class WordRestController {
 
-    @Autowired
-    WordRepository wordRepository;
-/*
-    @RequestMapping("/addWord")
-    public Word addWord(@RequestParam String word, @RequestParam String definition,
-                        @RequestParam String usedInSentence, @RequestParam String pronunciation) {
-
-        Word newWord = new Word(word, definition, usedInSentence, pronunciation);
-        wordRepository.save(newWord);
-
-        return newWord;
-    }
-    */
-
-    @PostMapping("/addWord")
-    public String addWord(@Valid Word word, BindingResult result){
-
-        if(result.hasErrors()){
-            return "Bad";
-        }
-        else{
-            wordRepository.save(word);
-            return "Good";
-        }
-    }
 }
