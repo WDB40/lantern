348e196b3f71abd82d432770850e18318e28f53b
WDB40 <wbrown1640@gmail.com>
2018-09-18 20:28:26 -0500

    Adding the API for getWordById

--- lantern-words/src/main/java/core/controller/WordRestController.java
+++ lantern-words/src/main/java/core/controller/WordRestController.java
@@ -6,12 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RequestMapping("/API")
 @RestController
 public class WordRestController {
+
+    @Autowired
+    WordRepository wordRepository;
+
+    @RequestMapping("/getWordById")
+    public Optional<Word> getWordByName(@RequestParam Integer id){
+
+        Optional<Word> foundWord = wordRepository.findById(id);
+
+        if(foundWord.isPresent()){
+            return foundWord;
+        }
+        else{
+            return Optional.empty();
+        }
+
+    }
 
 }
